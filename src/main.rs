e22ab8b0f40500a67ccf5629225cab49fc39f8ee
Andy Dirnberger <andy.dirnberger@gmail.com>
2026-02-20 23:48:11 -0500

    Add initial struct for deserialization (#1)
    There isn't much to see here. This adds the top-level struct that will
    be used to deserialize the JSON Claude passes through stdin.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt;
+use std::io;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ClaudeStatusLineData {}
+
+impl fmt::Display for ClaudeStatusLineData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data: ClaudeStatusLineData = serde_json::from_reader(io::stdin())?;
+    println!("{data}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn json_deserialization() {
+        let json = r#"{}"#;
+
+        let data: ClaudeStatusLineData = serde_json::from_str(json).unwrap();
+        // TODO: This test should be changed to check an attribute once ClaudeStatusLineData has
+        // one.
+        assert_eq!("", format!("{data}"));
+    }
 }
