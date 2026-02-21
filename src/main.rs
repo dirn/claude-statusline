0c3ef4726d8995c1fb1bb26f9e3472f67f8a3392
Andy Dirnberger <andy.dirnberger@gmail.com>
2026-02-21 00:07:13 -0500

    Add name of current model (#3)
    This is the first field being added to the statusline. As such, the TODO
    in the `json_deserialization` test is being addressed. There's a good
    chance this test will feel redundant when all is said and done.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use std::fmt;
 use std::io;
 
+use ansi_term::Color;
 use serde::Deserialize;
 
+// Colors
+const ORANGE: u8 = 208;
+
+// Icons
+const MODEL_ICON: &str = "🤖";
+
 #[derive(Deserialize)]
-struct ClaudeStatusLineData {}
+struct ClaudeStatusLineData {
+    model: Model,
+}
 
 impl fmt::Display for ClaudeStatusLineData {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "")
+        let model = &self.model;
+        write!(f, "{model}")
+    }
+}
+
+#[derive(Deserialize)]
+struct Model {
+    display_name: String,
+}
+
+impl fmt::Display for Model {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let display_name = Color::Fixed(ORANGE).paint(&self.display_name);
+        write!(f, "{MODEL_ICON} {display_name}")
     }
 }
 
@@ -23,12 +45,37 @@ mod tests {
     use super::*;
 
     #[test]
+    fn full_output() {
+        let data = ClaudeStatusLineData {
+            model: Model {
+                display_name: "Model Display Name".to_string(),
+            },
+        };
+
+        let output = format!("{data}");
+        assert!(output.contains("Model Display Name"));
+    }
+
+    #[test]
     fn json_deserialization() {
-        let json = r#"{}"#;
+        let json = r#"{
+            "model": {"display_name": "Sonnet 4.5"}
+        }"#;
 
         let data: ClaudeStatusLineData = serde_json::from_str(json).unwrap();
-        // TODO: This test should be changed to check an attribute once ClaudeStatusLineData has
-        // one.
-        assert_eq!("", format!("{data}"));
+        assert_eq!("Sonnet 4.5", data.model.display_name);
+    }
+
+    #[test]
+    fn model_output() {
+        let data = ClaudeStatusLineData {
+            model: Model {
+                display_name: "Model Display Name".to_string(),
+            },
+        };
+
+        let output = format!("{data}");
+        assert!(output.contains(MODEL_ICON));
+        assert!(output.contains("Model Display Name"));
     }
 }
